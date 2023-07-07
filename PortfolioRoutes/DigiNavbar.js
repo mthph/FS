cd2ea7fef3f186c7deb148a95c0601880bafd2f8
mthph <136421726+mthph@users.noreply.github.com>
2023-07-07 02:34:06 -0700

    Add files via upload

---
+++ PortfolioRoutes/DigiNavbar.js
@@ -0,0 +1,36 @@
+const DigiNavbar = () => {
+    
+    return (  
+
+<div class="navbar navbar-expand-sm navbar-dark text-light bg-warning">
+<div class="container">
+  <a class="navbar-brand" href="https://mpham.w3spaces.com/FSD/Exercise6/About.html">About</a>
+  <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+              <a class="nav-link" href="https://mpham.w3spaces.com/FSD/Exercise6/Academics.html" aria-current="page">Academics<span class="visually-hidden">(current)</span></a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="https://mpham.w3spaces.com/FSD/Exercise6/Technical_Skills.html">Technical Skills</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="https://mpham.w3spaces.com/FSD/Exercise6/Projects.html">Projects</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="https://mpham.w3spaces.com/FSD/Exercise6/Achievements.html">Achievements</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="https://mpham.w3spaces.com/FSD/Exercise6/Contact.html">Contact Me</a>
+          </li>
+          </ul>
+    </div>     
+  </div>
+</div>
+
+);
+    }
+export default DigiNavbar;
