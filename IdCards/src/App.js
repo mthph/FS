7438b3a51309556ba99779ab6417e7dac30ca3e1
mthph <136421726+mthph@users.noreply.github.com>
2023-07-13 19:35:39 -0700

    Add files via upload

---
+++ IdCards/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Idcards from './Idcards';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Welcome to IDCards App</h1>
+      {/* <Idcard/> */}
+      <Idcards/>
+    </div>
+  );
+}
+
+
+export default App;
